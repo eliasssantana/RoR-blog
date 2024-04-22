33cfad0a1f4f4b698857e31bb16aee9f9ef6f347
elias <eliassilva0045@gmail.com>
2024-04-22 15:31:05 -0300

    fix: add some new adjustments

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ user.update!(
     password: "1234567",
     password_confirmation: "1234567"
 )
+
+20.times do |i|
+    Post.create!(title: "Aliquam vitae odio neque #{i}", content: "Aliquam vitae odio neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam lacus dolor, interdum quis rhoncus sit amet, dapibus semper purus. Quisque commodo dolor sodales arcu mattis, sed gravida justo facilisis. Praesent sit amet scelerisque lacus, in mollis felis. Sed molestie sit amet felis accumsan placerat. In leo diam, maximus non pharetra sit amet, euismod eu sapien. Proin elementum ligula non ex posuere, vitae molestie nisi lacinia. Pellentesque commodo risus neque, tempus viverra lectus rutrum interdum.", user_id: user.id )
+end
